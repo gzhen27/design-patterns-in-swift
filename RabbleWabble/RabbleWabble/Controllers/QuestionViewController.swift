db9a28438ff5fab78cfc0ed4cfa200a90a946202
Gray <grayzprogrammer@gmail.com>
2022-08-10 10:15:20 -0500

    grouping files in the MVC pattern

---
+++ RabbleWabble/RabbleWabble/Controllers/QuestionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  QuestionViewController.swift
+//  RabbleWabble
+//
+//  Created by G Zhen on 8/9/22.
+//
+
+import UIKit
+
+public class QuestionViewController: UIViewController {
+
+    
+    
+}
+
