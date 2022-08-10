760c01de3444487c1bc373442ec1e102da5ea093
Gray <grayzprogrammer@gmail.com>
2022-08-09 21:20:39 -0500

    when user updates the view via IBAction, the Controller will update the Model

--- Model-View-Controller.playground/Contents.swift
+++ Model-View-Controller.playground/Contents.swift
@@ -22,7 +22,7 @@ public final class AddressViewController: UIViewController {
     
     // MARK: Properties
     public var address: Address? {
-        // when the Model being updates, Controller should also update the VIew
+        // when the Model being updates, Controller should also update the VIew.
         didSet {
             updateViewFromAddress()
         }
@@ -40,6 +40,24 @@ public final class AddressViewController: UIViewController {
         updateViewFromAddress()
     }
     
+    // MARK: - Actions
+    
+    // allow the user to update the address from the View,
+    // and then the Controller will update the Model.
+    @IBAction public func updateAddressFromView(_ sender: AnyObject) {
+        guard let street = addressView.streetTextField.text, street.count > 0,
+              let city = addressView.cityTextField.text, city.count > 0,
+              let state = addressView.stateTextField.text, state.count > 0,
+              let zipCode = addressView.zipCodeTextField.text, zipCode.count > 0 else {
+                  // TO-DO: display a message to handle the missing values.
+                  return
+              }
+           address = Address(street: street, city: city, state: state, zipCode: zipCode)
+    }
+    
+    /**
+     updates the View reflecing the updated data from the Model.
+     */
     private func updateViewFromAddress() {
         guard let addressView = addressView, let address = address else { return }
         addressView.streetTextField.text = address.street
