072fa7e28e3dcee7b06cf30e7248fdb6503901bd
Gray <grayzprogrammer@gmail.com>
2022-08-10 18:01:34 -0500

    built a controller and hook up with the view and model

--- RabbleWabble/RabbleWabble/Controllers/QuestionViewController.swift
+++ RabbleWabble/RabbleWabble/Controllers/QuestionViewController.swift
@@ -9,7 +9,33 @@ import UIKit
 
 public class QuestionViewController: UIViewController {
 
+    // MARK: - Instance Properties
+    public var questionGroup = QuestionGroup.basicPhrases()
+    public var questionIndex = 0
     
+    public var correctCount = 0
+    public var incorrectCount = 0
+    
+    public var questionView: QuestionView! {
+        guard isViewLoaded else { return nil}
+        return (view as! QuestionView)
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        showQuestion()
+    }
+    
+    private func showQuestion() {
+        let question = questionGroup.questions[questionIndex]
+        
+        questionView.answerLabel.text = question.answer
+        questionView.promptLabel.text = question.prompt
+        questionView.hintLabel.text = question.hint
+        
+        questionView.answerLabel.isHidden = true
+        questionView.hintLabel.isHidden = true
+    }
     
 }
 
