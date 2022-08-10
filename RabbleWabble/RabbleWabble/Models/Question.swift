babf3c5e967421195243ec416f5051746a8b8f2d
Gray <grayzprogrammer@gmail.com>
2022-08-10 10:26:32 -0500

    adds models

---
+++ RabbleWabble/RabbleWabble/Models/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  RabbleWabble
+//
+//  Created by G Zhen on 8/10/22.
+//
+
+import Foundation
+
+public struct Question {
+    public let answer: String
+    public let hint: String?
+    public let prompt: String
+}
---
+++ RabbleWabble/RabbleWabble/Models/QuestionGroup.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionGroup.swift
+//  RabbleWabble
+//
+//  Created by G Zhen on 8/10/22.
+//
+
+import Foundation
+
+public struct QuestionGroup {
+    public let questions: [Question]
+    public let title: String
+}
