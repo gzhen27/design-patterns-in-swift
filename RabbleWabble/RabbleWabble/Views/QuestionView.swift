ba12dd38c50fe6ac9424046da36ba5c2c0fab23a
Gray <grayzprogrammer@gmail.com>
2022-08-10 17:49:57 -0500

    creates a view and connects it with the main storyboard

---
+++ RabbleWabble/RabbleWabble/Views/QuestionView.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionView.swift
+//  RabbleWabble
+//
+//  Created by G Zhen on 8/10/22.
+//
+
+import UIKit
+
+public class QuestionView: UIView {
+    
+    @IBOutlet public var answerLabel: UILabel!
+    @IBOutlet public var correctCountLabel: UILabel!
+    @IBOutlet public var incorrectCountLabel: UILabel!
+    @IBOutlet public var promptLabel: UILabel!
+    @IBOutlet public var hintLabel: UILabel!
+    
+}
